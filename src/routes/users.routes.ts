8fee3b2ee63d46e34728008633afd6e1392e5156
TiagoAraujoDev <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-07-29 15:58:55 -0300

    Add route for user authentification

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 
+import authenticateUserController from "../modules/useCases/AuthenticateUser";
 import createUserController from "../modules/useCases/CreateUser";
 
 const userRoutes = Router();
 
 userRoutes.post("/", (request, response) => {
   return createUserController().handle(request, response);
+});
+userRoutes.post("/login", (request, response) => {
+  return authenticateUserController().handle(request, response);
 });
 
 export { userRoutes };
