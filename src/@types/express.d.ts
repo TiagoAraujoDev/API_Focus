18866ace192eee5b8d078d03ee576bba8b4d756f
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-12-18 19:43:30 +0000

    add: cors

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -2,6 +2,7 @@ import { User } from "../modules/accounts/infra/typeorm/entities/User";
 
 declare global {
   namespace Express {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     export interface Request {
       user: Partial<User>;
     }
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,14 +1,18 @@
 import "reflect-metadata";
+import cors from "cors";
 import express from "express";
 import "express-async-errors";
 
 import "../../container";
 
 import { createConnection } from "../typeorm";
+import { corsOptions } from "./config/cors/corsOptions";
 import { errorHandling } from "./middlewares/error";
 import { router } from "./routes";
 
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(router);
