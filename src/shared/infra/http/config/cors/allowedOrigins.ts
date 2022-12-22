c37d292c60bda748b447619195036cd893287dfd
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-12-22 11:07:24 +0000

    Update allowedOrigins.ts

--- src/shared/infra/http/config/cors/allowedOrigins.ts
+++ src/shared/infra/http/config/cors/allowedOrigins.ts
@@ -1,5 +1,7 @@
 export const allowedOrigins = [
   "https://focus-pi.vercel.app",
   "http://127.0.0.1:5173",
-  "http://localhost:8080",
+  "http://127.0.0.1:5173/",
+  "http://localhost:5173",
+  "http://localhost:5173/",
 ];
