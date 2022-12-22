ae22450e3e5c5a82e17a81eb870e1b5711111ae0
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-12-22 11:52:39 +0000

    Update corsOptions.ts

--- src/shared/infra/http/config/cors/corsOptions.ts
+++ src/shared/infra/http/config/cors/corsOptions.ts
@@ -5,9 +5,10 @@ import { allowedOrigins } from "./allowedOrigins";
 export const corsOptions: CorsOptions = {
   origin(origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      return callback(null, true);
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS!"));
     }
-    return callback(new Error("Not allowed by CORS!"));
   },
   optionsSuccessStatus: 200,
 };
