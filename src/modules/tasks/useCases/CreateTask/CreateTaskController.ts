7931eab09a20b8ee2fe08a5a6e5b7e913aaedbdf
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-12-28 17:04:02 +0000

    fix

--- src/modules/tasks/useCases/CreateTask/CreateTaskController.ts
+++ src/modules/tasks/useCases/CreateTask/CreateTaskController.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
+import { AppError } from "../../../../shared/errors/AppError";
 import { CreateTaskUseCase } from "./CreateTaskUseCase";
 
 class CreateTaskController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { title } = request.body;
     const { id: user_id } = request.user;
+
+    if (!title) {
+      throw new AppError("Missing title!");
+    }
 
     const createTaskUseCase = container.resolve(CreateTaskUseCase);
     await createTaskUseCase.execute({ title, user_id });
--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,6 +1,5 @@
 export class AppError {
   public readonly statusCode: number;
-
   public readonly message: string;
 
   constructor(message: string, statusCode = 400) {
