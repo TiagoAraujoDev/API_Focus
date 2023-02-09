66e3c313a3efeb506bff316cd0daa241de10868a
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-02-09 22:56:31 +0000

    add: multer

---
+++ src/config/multer.ts
@@ -0,0 +1,18 @@
+import multer from "multer";
+import crypto from "node:crypto";
+import path from "node:path";
+
+const tempFolder = path.join(__dirname, "..", "..", "tmp");
+
+export const multerConfig = {
+  tempFolder,
+  storage: multer.diskStorage({
+    destination: tempFolder,
+    filename: (request, file, callback) => {
+      const fileHash = crypto.randomBytes(16).toString("hex");
+      const fileName = `${fileHash}-${file.filename}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
